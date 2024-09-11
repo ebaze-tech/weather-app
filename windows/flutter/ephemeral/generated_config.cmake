319436e354f807b57c5b1981322b73389edd4472
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-09-11 16:30:37 +0100

    Started development

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\TOSHIBA\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\TOSHIBA\\Desktop\\MyProjects\\dart_and_flutter\\weather_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\TOSHIBA\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\TOSHIBA\\Desktop\\MyProjects\\dart_and_flutter\\weather_app"
+  "FLUTTER_ROOT=C:\\Users\\TOSHIBA\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\TOSHIBA\\Desktop\\MyProjects\\dart_and_flutter\\weather_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\TOSHIBA\\Desktop\\MyProjects\\dart_and_flutter\\weather_app"
+  "FLUTTER_TARGET=C:\\Users\\TOSHIBA\\Desktop\\MyProjects\\dart_and_flutter\\weather_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\TOSHIBA\\Desktop\\MyProjects\\dart_and_flutter\\weather_app\\.dart_tool\\package_config.json"
+)
